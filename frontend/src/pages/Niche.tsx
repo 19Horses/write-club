76abcf35b48338af8a1a7571151a5c1a0e6c278f
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-10-29 20:10:18 +0000

    Niche for now

--- frontend/src/pages/Niche.tsx
+++ frontend/src/pages/Niche.tsx
@@ -1,5 +1,18 @@
 import { Header } from '../components/Header';
+import { Layout } from '../components/Layout';
+import { BodyText } from '../styling/styles';
 
 export const Niche = () => {
-  return <Header title="What's your Niche?" />;
+  return (
+    <>
+      <Header title="What's your Niche?" />
+      <Layout>
+        <BodyText>
+          Interviews with a range of people from different fields providing
+          insight into the minds of some of this generations intellectual and
+          creative thinkers and talents.
+        </BodyText>
+      </Layout>
+    </>
+  );
 };
